57cdad51bc87675a2eba4cf28d0f88aeab9c4baa
kadirgulteekin <kadirgulteekin@gmail.com>
2023-09-27 22:51:08 +0300

    CategoriesController

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,43 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CategoriesController : CustomBaseController
+    {
+
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            return CreateActionResultInstance(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var categories = await _categoryService.GetByIdAsnc(id);
+            return CreateActionResultInstance(categories);
+        }
+
+        public async Task<IActionResult> Created(CategoryDto categoryDto)
+        {
+            var categories = await _categoryService.CreateAsync(categoryDto);
+            return CreateActionResultInstance(categories);
+        }
+
+
+    }
+}
